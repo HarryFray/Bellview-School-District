c1f95805df42fae473c0cd67bacfa5cbbd16496e
HarryFray <harry.fray7@gmail.com>
2017-11-02 09:47:21 -0700

    Comments/ future goals
    adding comments to help explain code and what i want it to do in the future

--- Bellview School District.py
+++ Bellview School District.py
@@ -1,6 +1,9 @@
+
+
 class SchoolDistrict(object):
     weights = [.2, .2, .2, .4]
     total_student = 0
+    ''' initializes name, grades, gender and name to be used in the following methods and future nested classes '''
     def __init__(self,name,gender,qgrade1,qgrade2,qgrade3,final):
         self.name = name
         self.gender = gender
@@ -9,7 +12,8 @@ class SchoolDistrict(object):
         self.qgrade3 = qgrade3
         self.final = final
         SchoolDistrict.total_student += 1
-
+''' inputs are grades outputs the weighted averages of the students as well as there letter grade the weight is a class variable found 
+above future code will allow for a non specific number of grades to be inputed...quizes homework extra credit ect. '''
     def year_grade(self):
         weighted_score = self.qgrade1*self.weights[0] + self.qgrade2*self.weights[1] \
                          + self.qgrade3*self.weights[2] + self.final*self.weights[3]
@@ -31,7 +35,7 @@ class HighSchool(SchoolDistrict):
         pass
 
 
-
+''' future code will pull random test scores based on random int, this will come from a seperate file '''
 # import randint from random
 kid1 = SchoolDistrict('nick','male',54,90,55,100)
 kid2 = SchoolDistrict('joe','male',70,90,100,100)
